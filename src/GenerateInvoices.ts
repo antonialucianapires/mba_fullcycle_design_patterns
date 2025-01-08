cffb42823e9b8e6fa18b75fe4e3a5e045ef887bd
antonialucianapires <antonialucianapires@gmail.com>
2025-01-08 12:49:46 -0300

    feat: caso de uso para gerar notas fiscais por meio de regime de competência

--- src/GenerateInvoices.ts
+++ src/GenerateInvoices.ts
@@ -7,16 +7,30 @@ export default class GenerateInvoices {
         const contracts = await connection.query("SELECT * FROM branas.contract");
         const output: Output[] = [];
         for (const contract of contracts) {
-            const payments = await connection.query("SELECT * FROM branas.payment WHERE id_contract = $1", [contract.id_contract]);
-            for (const payment of payments) {
-                if (payment.date.getMonth() + 1 !== input.month || payment.date.getFullYear() !== input.year) {
-                    continue;
+            if (input.type === "cash") {
+                const payments = await connection.query("SELECT * FROM branas.payment WHERE id_contract = $1", [contract.id_contract]);
+                for (const payment of payments) {
+                    if (payment.date.getMonth() + 1 !== input.month || payment.date.getFullYear() !== input.year) {
+                        continue;
+                    }
+                    output.push({
+                        date: moment(payment.date).format("YYYY-MM-DD"),
+                        amount: parseFloat(payment.amount)
+                    });
                 }
-                output.push({
-                    date: moment(payment.date).format("YYYY-MM-DD"),
-                    amount: parseFloat(payment.amount)
-                });
             }
+
+            if (input.type === "accrual") {
+                let period = 0;
+                while (period <= contract.periods) {
+                    const date = moment(contract.date).add(period++, "months").toDate();
+                    if (date.getMonth() + 1 !== input.month || date.getFullYear() !== input.year) continue;
+                    const amount = parseFloat(contract.amount) / contract.periods;
+                    output.push({ date: moment(date).format("YYYY-MM-DD"), amount });
+                }
+
+            }
+
         }
         await connection.$pool.end();
         return output;
@@ -26,6 +40,7 @@ export default class GenerateInvoices {
 type Input = {
     month: number;
     year: number;
+    type: string;
 }
 
 type Output = {
--- test/GenerateInvoices.test.ts
+++ test/GenerateInvoices.test.ts
@@ -11,3 +11,15 @@ test("Deve gerar as notas fiscais por regime de caixa", async function () {
     expect(output.at(0)?.date).toBe("2022-01-05");
     expect(output.at(0)?.amount).toBe(6000);
 });
+
+test("Deve gerar as notas fiscais por regime de competência", async function () {
+    const generateInvoices = new GenerateInvoices();
+    const input = {
+        month: 1,
+        year: 2022,
+        type: "accrual"
+    }
+    const output = await generateInvoices.execute(input);
+    expect(output.at(0)?.date).toBe("2022-01-01");
+    expect(output.at(0)?.amount).toBe(500);
+});
